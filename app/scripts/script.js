26e2079559c85f87d803082d1ad6bc8d2fb553a3
loci <locisvv@gmail.com>
2015-02-18 22:49:04 +0000

    fixed mobile parallax

--- app/scripts/script.js
+++ app/scripts/script.js
@@ -1,4 +1,14 @@
-$(function () { 
+$(function () {
+  if (navigator.userAgent.match(/(iPod|iPhone|iPad|Android)/)) {
+    $('#ios-notice').removeClass('hidden');
+    $('.parallax-container').height( $(window).height() * 0.5 | 0 );
+  } else {
+    $(window).resize(function () {
+      var parallaxHeight = Math.max($(window).height() * 0.7, 200) | 0;
+      $('.parallax-container').height(parallaxHeight);
+    }).trigger('resize');
+  }
+  
   smoothScroll.init();
 });
 
